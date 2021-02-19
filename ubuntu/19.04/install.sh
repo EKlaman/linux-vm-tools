a1b1ec9a54fdf3c8437966998c6dedf3435521bb
Yutaro-B18016 <e1b18016@oit.ac.jp>
2021-02-19 20:07:27 +0900

    added requirement for dependancies for Ubuntu Base

--- ubuntu/19.04/install.sh
+++ ubuntu/19.04/install.sh
@@ -33,7 +33,7 @@ apt install -y linux-tools-virtual
 apt install -y linux-cloud-tools-virtual
 
 # Install the xrdp service so we have the auto start behavior
-apt install -y xrdp
+apt install -y xrdp xserver-xorg-core xorgxrdp
 
 systemctl stop xrdp
 systemctl stop xrdp-sesman
